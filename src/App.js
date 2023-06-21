1dceb185763b4c90e9f4c5ee86702dc06e4da5fd
Manoranjan710 <manoranjan710@gmail.com>
2023-06-22 00:04:31 +0530

    final update

--- src/App.js
+++ src/App.js
@@ -8,17 +8,25 @@ import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
 import getFormattedWeatherData from './services/weatherService'
 import { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-
+    
   const [query, setQuery] = useState({q: 'berlin'})
   const [units, setUnits] = useState('metric')
   const [weather, setWeather] = useState(null)
 
   useEffect(()=>{
+
     const fetchWeather = async () =>{
+      const message = query.q ? query.q : 'current location.';
+
       await getFormattedWeatherData({...query, units}).then(
         (data) =>{
+          
+          toast.success(`Successfully fetched weather for ${data.name}, ${data.country}.`)
+
           setWeather(data)
         }
       );
@@ -31,8 +39,8 @@ function App() {
 
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButtons/>
-      <Inputs/>
+      <TopButtons setQuery={setQuery}/>
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/>
       {weather && (
       <div>
         <TimeAndLocation weather={weather} />
@@ -43,8 +51,9 @@ function App() {
       </div>
       )}
 
-      
+      <ToastContainer autoClose={3000} theme='colored' newestOnTop={true} />
     </div>
+    
   );
 }
 
