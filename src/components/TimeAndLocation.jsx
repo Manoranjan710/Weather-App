af723a82a29049449d47186c6dcda586657f7d7c
Manoranjan710 <manoranjan710@gmail.com>
2023-06-21 15:49:19 +0530

    code updated

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { formatToLocalTime } from '../services/weatherService';
 
-function TimeAndLocation() {
+function TimeAndLocation({weather: {dt, timezone, name, country}}) {
   return (
     <div>
         <div className='flex items-center justify-center my-6'>
             <p className='text-white text-xl font-extralight'>
-                 Monday, 19 June 2023 | Local time: 12:24 PM
+                 {formatToLocalTime(dt, timezone)}
             </p>
         </div>
         <div className='flex items-center justify-center my-3'>
             <p className=' text-white text-3xl font-medium'>
-                Berlin, DE 
+                {`${name}, ${country}`}
             </p>
         </div>
     </div>
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -42,7 +42,7 @@ const formatForecastWeather = (data) => {
     hourly = hourly?.slice(1,6)?.map((d) => {
         return ({
             title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
-            temp: d?.temp?.day,
+            temp: d?.temp,
             icon: d?.weather[0]?.icon
         });
     });
@@ -64,4 +64,8 @@ const getFormattedWeatherData = async (searchParams) =>{
 
 const formatToLocalTime = (secs, zone, format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
-export default getFormattedWeatherData
+const iconUrlFromCode = (code) => `https://openweathermap.org/img/wn/${code}@2x.png`
+
+export default getFormattedWeatherData;
+
+export { formatToLocalTime, iconUrlFromCode};
